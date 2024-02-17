aaef0d0bcdf1fc90ae27df6f327cc846a2393342
Mo Aziz <mido85only@gmail.com>
2024-02-17 20:39:11 +0200

    Initial Commit

---
+++ ImageHandlingApp/ImageHandlingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageHandlingAppApp.swift
+//  ImageHandlingApp
+//
+//  Created by mohammed on 17/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ImageHandlingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
